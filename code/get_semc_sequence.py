9634ca9a4e708c6373f0580e06c0813452f617a2
7huahua <892093449@qq.com>
2023-04-27 15:34:53 +0900

    change sample

--- code/get_semc_sequence.py
+++ code/get_semc_sequence.py
@@ -6,12 +6,14 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import random
 import seaborn as sns
+from sklearn.metrics import silhouette_score
 
 class SemcSequence(object):
     # init
     def __init__(self,stayregions=None):
         self.stayregions = self.set_day_week_weekday(stayregions)
-        self.stayregions = self.cluster_stayregions(self.stayregions,'category1','cluster_id')
+        # k of k-means is set to 35 the same as the other paper, you can change it to other values
+        self.stayregions = self.cluster_stayregions(self.stayregions,'category1','cluster_id',k=35)
         # get stay region sequence
         self.semc_sequences = self.get_stayregion_sequence(self.stayregions)
 
@@ -26,65 +28,45 @@ class SemcSequence(object):
     # get poi tfidf
     def get_poi_tfidf(self,stayregions,poi_label):
         # for each stay region, treat it as a phase, and the pois within it as words
-        # get the tfidf of each phase
-        # 对于每一行的poi str，在使用分号分割后，用空格连接起来，形成一个字符串
-        # 首先，对于每一行的poi str，使用分号分割，得到一个list
-        # 然后，对于每一个list中的元素，使用空格连接起来，得到一个字符串
-        # 最后，将所有的字符串连接起来，得到一个大的字符串
-
-        # 注意，这里poi内部的元素中包含''以及'未知'，需要去除
         corpus = [' '.join(poi.split(';')) for poi in stayregions[poi_label]]
         # print(corpus)
+        # filter some stop words
         custom_stop_words = ['life','未知']
-        # 使用tfidf对每一个stay region进行向量化
         vectorizer = TfidfVectorizer(stop_words = custom_stop_words,token_pattern='(?u)\\b\\w+\\b')
         sparse_matrix = vectorizer.fit_transform(corpus)
-        # 输出词袋模型中的所有词
-        word = vectorizer.get_feature_names_out()
-        print(word)
-        # sparse_matrix is a csr_matrix
-        # print(type(sparse_matrix))
-        
-        print(sparse_matrix.shape)
+        # print all the words
+        # word = vectorizer.get_feature_names_out()
+        # print(word)
+        # print(sparse_matrix.shape)
 
         return sparse_matrix
 
     # cluster stay regions by poi tfidf
-    def cluster_stayregions(self,stayregions,poi_label,cluster_label):
+    def cluster_stayregions(self,stayregions,poi_label,cluster_label, k):
         # get poi tfidf
         poi_tfidf = self.get_poi_tfidf(stayregions,poi_label)
-        # poi_tfidf is a csr_matrix
-        # print(poi_tfidf)
+        
         # cluster stay regions by poi tfidf value using kmeans
-        kmeans = KMeans(n_clusters=35, random_state=0).fit(poi_tfidf)
+        kmeans = KMeans(n_clusters=k, random_state=0).fit(poi_tfidf)
         stayregions[cluster_label] = kmeans.labels_
 
-        # print cluster labels
-        # print(stayregions[cluster_label].unique())
-
         # print cluster label and its count
-        # print(stayregions[cluster_label].value_counts()) 比较均匀
+        # print(stayregions[cluster_label].value_counts()) # you can check the distribution of the cluster result here 
 
         # visualize cluster result, using the matrix of tfidf
-        self.visualize_cluster_result_2D(poi_tfidf.toarray(),stayregions[cluster_label].tolist())
+        # self.visualize_cluster_result_2D(poi_tfidf.toarray(),stayregions[cluster_label].tolist())
         # self.visualize_cluster_result_3D(poi_tfidf.toarray(),stayregions[cluster_label].tolist())
-        
-        
         
         return stayregions
     
     def visualize_cluster_result_2D(self,vectors,labels):
-        # 假设 vectors 是一个向量列表, labels 是一个标签列表
-        # 定义一个TSNE模型
+
         tsne_model = TSNE(n_components=2, perplexity=40, learning_rate=100, n_iter=500)
 
-        # 将高维向量映射到二维空间
         tsne_vectors = tsne_model.fit_transform(vectors)
 
-        # 使用matplotlib进行可视化
         unique_labels = list(set(labels))
-        # colors = ['#'+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(26)]
-        # 使用 seaborn 生成颜色
+
         colors = sns.color_palette("husl", len(unique_labels))
         for i, label in enumerate(unique_labels):
             x = tsne_vectors[np.where(np.array(labels) == label), 0]
@@ -94,14 +76,11 @@ class SemcSequence(object):
         plt.legend()
         plt.show()
 
-
-
     def visualize_cluster_result_3D(self,vectors,labels):
-        # 将向量降维到三维空间
+
         tsne_model = TSNE(n_components=3, perplexity=40, learning_rate=100, n_iter=500)
         tsne_vectors = tsne_model.fit_transform(vectors)
 
-        # 使用matplotlib进行可视化
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
@@ -185,7 +164,7 @@ class SemcSequence(object):
 
 if __name__ == "__main__":
     # first get stay regions
-    stayregions = pd.read_csv('data/combined_poi.csv',parse_dates=['arr_t','lea_t'])
+    stayregions = pd.read_csv('data/user_profile/stayregions_sample.csv',parse_dates=['arr_t','lea_t'])
     # only remain rows with value of category1 or category2
     stayregions = stayregions[stayregions['category1'].notnull() | stayregions['category2'].notnull()]
 
